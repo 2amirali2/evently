d94f7c1627879d162b010511f845c805fc0f23e5
aamigo <aamigodeveloper@gmail.com>
2024-06-04 23:06:06 +0330

    again

--- app/api/auth/creation/route.ts
+++ app/api/auth/creation/route.ts
@@ -5,7 +5,7 @@ import { NextResponse } from "next/server"
 
 export async function GET() {
   // try {
-    connectToDB()
+    await connectToDB()
     const { getUser } = getKindeServerSession()
 
     const user = await getUser()
